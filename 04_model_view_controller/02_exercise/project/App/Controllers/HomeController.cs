1bcbd0d8bcc6aaba19b0309a78536fd046b97572
pawel <pawcio27610@wp.pl>
2020-11-21 12:13:21 +0100

    zad 4.1 done

--- 04_model_view_controller/02_exercise/project/App/Controllers/HomeController.cs
+++ 04_model_view_controller/02_exercise/project/App/Controllers/HomeController.cs
@@ -1,22 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+// using Microsoft.Extensions.Logging;
 using App.Models;
 
 namespace App.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        // private readonly ILogger<HomeController> _logger;
+        //
+        // public HomeController(ILogger<HomeController> logger)
+        // {
+        //     _logger = logger;
+        // }
 
         public IActionResult Index()
         {
--- 04_model_view_controller/02_exercise/project/App/Startup.cs
+++ 04_model_view_controller/02_exercise/project/App/Startup.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using App.Data;
 using Microsoft.Extensions.Configuration;
@@ -22,7 +16,7 @@ namespace App
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        private IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -61,8 +55,8 @@ namespace App
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
