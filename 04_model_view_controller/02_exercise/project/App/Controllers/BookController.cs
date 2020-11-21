d4d0caee5b7b8348a2e1f8168be9fb0c2c378882
pawel <pawcio27610@wp.pl>
2020-11-21 23:29:00 +0100

    add adding books

--- 04_model_view_controller/02_exercise/project/App/Controllers/BookController.cs
+++ 04_model_view_controller/02_exercise/project/App/Controllers/BookController.cs
@@ -1,11 +1,13 @@
 using System.Linq;
 using App.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-//using Utils;
+using Utils;
 
 namespace App.Controllers
 {
+    [Authorize]
     public class BookController:Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -17,24 +19,45 @@ namespace App.Controllers
             _context = context;
         }
         
+        [HttpGet]
         public IActionResult Index()
         {
             // var book = new Book { Title = "tytul",Description = "opis"};
             // _context.Books?.Add(book);
             // _context.SaveChanges();
+            // var list = _context.Books?.ToList();
+            // if (list != null)
+            // {
+            //     foreach (var item in list.Where(item => string.IsNullOrEmpty(item.Title)))
+            //     {
+            //         _context.Books?.Remove(item);
+            //     }
+            //
+            //     _context.SaveChanges();
+            // }
+
             _logger.LogInformation("BookController Index()");
             return View(_context.Books?.ToList());
         }
-
+        
         public IActionResult Create() // - displays a form for creating a new book
         {
             _logger.LogInformation("BookController Create()");
             return View();
-        }
+            }
 
-        public void Store() // - saves the book in the database - redirection to Index after adding or to Store after error
+        [HttpPost]        
+        public IActionResult Store() // - saves the book in the database - redirection to Index after adding or to Store after error
         {
-            
+            var title = Request.Form["Title"];
+            var description = Request.Form["Description"];
+            if (string.IsNullOrEmpty(title) || string.IsNullOrEmpty(description))
+            {
+                return RedirectToAction("Create");
+            }
+            _context.Books?.Add(new Book{Title = title,Description = description});
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
