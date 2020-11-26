a38d39ee31d89257ef7dcd9bf47258573e02d41d
pawel <pawcio27610@wp.pl>
2020-11-26 11:10:23 +0100

    repair of pipelines

--- 05_grpc/02_exercise/project/Server/Startup.cs
+++ 05_grpc/02_exercise/project/Server/Startup.cs
@@ -32,10 +32,10 @@ namespace Server
             {
                 endpoints.MapGrpcService<GreeterService>();
 
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync( "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909").ConfigureAwait(false);
-                });
+                // endpoints.MapGet("/", async context =>
+                // {
+                //     await context.Response.WriteAsync( "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909").ConfigureAwait(false);
+                // });
             });
         }
     }
